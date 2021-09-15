5d029bd232431626bafe3bf28d2cb7514029e566
Navleen <navleenkaurr@gmail.com>
2021-09-15 16:35:26 -0400

    Added view routes

--- controllers/viewRoutes.js
+++ controllers/viewRoutes.js
@@ -1,5 +1,38 @@
 const router = require('express').Router();
 
+router.get('/', async (req, res) => {
+    res.render('homepage');
+});
 
+router.get('/login', async (req, res) => {
+    res.render('login');
+});
+
+router.get('/signup', async (req, res) => {
+    res.render('signup');
+});
+
+router.get('/search', async (req, res) => {
+    res.render('search');
+});
+
+router.get('/search/youtube', async (req, res) => {
+    res.render('youtube');
+});
+
+router.get('/search/reddit', async (req, res) => {
+    res.render('reddit');
+});
+
+router.get('/search/stackoverflow', async (req, res) => {
+    res.render('stackoverflow');
+});
+router.get('/favorites', async (req, res) => {
+    res.render('favorites');
+});
+
+router.get('/code-editor', async (req, res) => {
+    res.render('code-editor');
+});
 
 module.exports = router;
