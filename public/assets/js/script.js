667e7980a2f43380474dea906bdcf3958e7d2344
Navleen <navleenkaurr@gmail.com>
2021-09-17 15:02:36 -0400

    Set up more buttons

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -7,3 +7,24 @@ searchButton.addEventListener('click', function(e) {
     const searchValue = searchInput.value;
     window.location.assign("/search?search=" + searchValue);
 });
+
+if (window.location.pathname == "/search") {
+    const youtubeMoreBtn = document.querySelector('#youtube-more');
+    const stackMoreBtn = document.querySelector('#stack-more');
+    const redditMoreBtn = document.querySelector('#reddit-more');
+
+    youtubeMoreBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.assign("/search/youtube" + window.location.search);
+    });
+
+    stackMoreBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.assign("/search/stackoverflow" + window.location.search);
+    });
+
+    redditMoreBtn.addEventListener('click', function(e) {
+        e.preventDefault();
+        window.location.assign("/search/reddit" + window.location.search);
+    });
+};
