a265a43a545249b9939aed5f400d825327728c7f
Navleen <navleenkaurr@gmail.com>
2021-09-17 15:19:43 -0400

    Fixed button bug

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,11 +2,13 @@
 const searchInput = document.querySelector('.search-input');
 const searchButton = document.querySelector('.search-btn');
 
-searchButton.addEventListener('click', function(e) {
-    e.preventDefault();
-    const searchValue = searchInput.value;
-    window.location.assign("/search?search=" + searchValue);
-});
+if (searchButton != null) {
+    searchButton.addEventListener('click', function (e) {
+        e.preventDefault();
+        const searchValue = searchInput.value;
+        window.location.assign("/search?search=" + searchValue);
+    });
+};
 
 if (window.location.pathname == "/search") {
     const moreBtns = document.querySelectorAll('.moreBtn');
