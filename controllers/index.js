192b48201165edaf90a39641f0b7fd27b21610b5
Navleen <navleenkaurr@gmail.com>
2021-09-21 14:33:50 -0400

    Added api folder to controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
 const viewRoutes = require('./viewRoutes');
 const searchRoutes = require('./searchRoutes');
 
@@ -7,6 +8,7 @@ const signupRoutes = require("./signup-routes");
 const logoutRoutes = require("./logout-routes");
 
 router.use('/', viewRoutes);
+router.use('/api', apiRoutes);
 router.use('/api/search', searchRoutes);
 
 router.use("/login", loginRoutes);
