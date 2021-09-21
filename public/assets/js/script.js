79e54baa6349181bb081126dce6e58dc82573800
Navleen <navleenkaurr@gmail.com>
2021-09-21 13:28:30 -0400

    Fixed search so it doesn't search with no input

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-inner-declarations */
 const searchInput = document.querySelector('.search-input');
 const searchButton = document.querySelector('.search-btn');
 
@@ -7,10 +9,12 @@ if (searchButton != null) {
     searchButton.addEventListener('click', function (e) {
         e.preventDefault();
         const searchValue = searchInput.value;
+        if (searchValue != null) {
         // changes url to go to search results page
         window.location.assign("/search?search=" + searchValue);
+        }
     });
-};
+}
 
 // Search page more buttons functionality
 if (window.location.pathname == "/search") {
@@ -25,7 +29,7 @@ if (window.location.pathname == "/search") {
             window.location.assign("/search/" + moreBtn.dataset.source + window.location.search);
         });
     });
-};
+}
 
 // Following is to run the code-editor
 if (window.location.pathname == "/code-editor") {
@@ -73,7 +77,7 @@ if (window.location.pathname == "/code-editor") {
     });
 
     window.MonacoEnvironment = {
-        getWorkerUrl: function (workerId, label) {
+        getWorkerUrl: function () {
             return `data:text/javascript;charset=utf-8,${encodeURIComponent(`
             self.MonacoEnvironment = {
               baseUrl: 'https://cdn.jsdelivr.net/npm/monaco-editor/min/'
