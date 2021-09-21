4751c00f7c48458d1d08481edf204645730de7aa
pgpguru <66192479+pgpguru@users.noreply.github.com>
2021-09-20 20:31:08 -0400

    updated 2
    updated 2

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const viewRoutes = require('./viewRoutes');
-const searchRoutes = require('./searchRoutes');
+const loginRoutes = require("./login-routes");
+const signupRoutes = require("./signup-routes");
+const logoutRoutes = require("./logout-routes");
+router.use('./searchroutes', searchRoutes);
+
+
 
 router.use('/', viewRoutes);
-router.use('/api/search', searchRoutes);
+router.use("/login", loginRoutes);
+router.use("/signup", signupRoutes);
+router.use("/logout", logoutRoutes);
+router.use('/api/searchAPI.', searchRoutes);
 
 module.exports = router;
