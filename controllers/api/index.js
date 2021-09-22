51f640a6e56645cd129ad899e958525f557b92e1
Navleen <navleenkaurr@gmail.com>
2021-09-22 14:28:36 -0400

    Moved searchRoutes to api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const searchRoutes = require('./searchRoutes');
 
+router.use('/search', searchRoutes);
 router.use('/user', userRoutes);
 
 module.exports = router;
